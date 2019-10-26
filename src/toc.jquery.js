16ee024e01f630f3b42f74aa054dc345c0254955
Huynhhuynh <conghieuhuynh91@gmail.com>
2019-10-26 15:59:44 +0700

    update select list & responsive

--- src/toc.jquery.js
+++ src/toc.jquery.js
@@ -15,7 +15,7 @@
         self.settings = $.extend( {
             target: 'h1,h2,h3,h4,h5,h6',
             default_text: 'Select table of content',
-            content_witdh: 700,
+            content_witdh: 680,
         }, settings );
 
         self.target_list = self.$container.find( self.settings.target );
@@ -26,7 +26,12 @@
         /**
          * Append toc tool bar
          */
-        $( 'body' ).append( self.$toc_sticky_tool );
+        $( 'body' ).append( [self.$toc_sticky_tool, self.$select] );
+
+        /**
+         * Select handle
+         */
+        this.select_handle();
 
         /**
          * Event scroll handler
@@ -42,24 +47,26 @@
 
         self.$nav = $( '<div>', {
             class: 'toc-nav'
-        } )
+        } );
 
-        self.$current = $( '<div>', {
-            class: 'toc-current',
-            html: self.settings.default_text
-        } )
+        self.$current = $( `<div class="toc-current">
+            <div class="text">
+                ${ self.settings.default_text }
+            </div>
+        </div>` );
 
         self.target_list.each( function( index, item ) {
 
             var rand_key = `toc-key-${ Math.random().toString(36).substr(2, 5) }`;
+            var num_increase = index + 1;
 
             $( item ).data( 'toc-key', rand_key ).addClass( rand_key );
-            self.$select.append( `<li data-toc-key="${ rand_key }">${ $( item ).text() }</li>` );
+            self.$select.append( `<li data-toc-key="${ rand_key }">${ num_increase }. ${ $( item ).text() }</li>` );
             self.$nav.append( `<div class="item" data-toc-key="${ rand_key }"><span></span></div>` );
         } )
 
-        var $temp = $( `<div class="toc-sticky-container">
-            <div class="toc-sticky-container__inner" style="width: ${ self.settings.content_witdh }px;">
+        var $temp = $( `<div class="toc-sticky-container" style="--toc-content-width: ${ self.settings.content_witdh }px; ">
+            <div class="toc-sticky-container__inner">
                 <div class="toc-summary"></div>
             </div>
         </div>`, {
@@ -73,6 +80,35 @@
 
         $temp.find( '.toc-summary' ).append( [self.$nav, self.$current] );
         return $temp;
+    }
+
+    toc.prototype.select_handle = function() {
+        var self = this;
+
+        $( 'body' ).on( 'click', function( e ) {
+
+            if( $( '.__is-select-show' ).length > 0 && $( e.target ).parents( '.toc-select' ).length == 0 ) {
+
+                $( 'body' ).removeClass( '__is-select-show' );
+            }
+        } )
+
+        self.$current.on( 'click', function( e ) {
+            e.preventDefault();
+            e.stopPropagation();
+
+            $( 'body' ).toggleClass( '__is-select-show' )
+        } )
+
+        self.$select.on( 'click', 'li', function( e ) {
+            var key = $( this ).data( 'toc-key' );
+
+            $( 'body, html' ).animate( {
+                scrollTop: self.$container.find( `.${ key }` ).offset().top + 1,
+            }, 300, function() {
+                $( 'body' ).removeClass( '__is-select-show' );
+            } );
+        } )
     }
 
     toc.prototype.scroll_handler = function() {
@@ -120,13 +156,15 @@
                     $body.addClass( 'toc-show' );
                     var data = in_content();
 
-                    self.$nav
-                        .find( `[data-toc-key="${ data.key }"]` )
-                        .addClass( '__is-active' )
-                        .siblings()
-                        .removeClass( '__is-active' );
+                    [self.$nav, self.$select].forEach( function( $item, index  ) {
+                        $item
+                            .find( `[data-toc-key="${ data.key }"]` )
+                            .addClass( '__is-active' )
+                            .siblings()
+                            .removeClass( '__is-active' );
+                    } )
 
-                    self.$current.text( data.text );
+                    self.$current.find( '.text' ).text( data.text );
                 } else {
                     $body.removeClass( 'toc-show' );
                 }
