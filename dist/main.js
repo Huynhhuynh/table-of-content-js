9797e5022e861601849ecae964bcfb0716d0b312
Huynhhuynh <conghieuhuynh91@gmail.com>
2019-10-31 15:39:44 +0700

    AMP setup

--- dist/main.js
+++ dist/main.js
@@ -1,16 +1,37 @@
 /**
  *
  */
-jQuery( function() {
-    new window.table_of_content( jQuery( '#main' ), {
-        on_show ( key ) {
-            // console.log( this, key );
-        },
-        on_hide () {
-            // console.log( this, key );
-        },
-        on_change ( key ) {
-            // console.log( this, key );
-        },
-    } );
+
+var load_script_func = function( url, callback ) {
+    var script = document.createElement('script');
+    script.src = url;
+    script.type = 'text/javascript';
+
+    script.onload = function() {
+        callback.call( this );
+    };
+
+    document.getElementsByTagName('head')[0].appendChild(script);
+}
+
+load_script_func( 'https://cdnjs.cloudflare.com/ajax/libs/jquery/1.12.4/jquery.min.js', function() {
+
+    load_script_func( 'https://huynhhuynh.github.io/table-of-content-js/src/toc.jquery.js', function() {
+
+        var $ = window.jQuery;
+
+        $( function() {
+            new window.table_of_content( $( '#main' ), {
+                on_show ( key ) {
+                    // console.log( this, key );
+                },
+                on_hide () {
+                    // console.log( this, key );
+                },
+                on_change ( key ) {
+                    // console.log( this, key );
+                },
+            } );
+        } )
+    } )
 } )
